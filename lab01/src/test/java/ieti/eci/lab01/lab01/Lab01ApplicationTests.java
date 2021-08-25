4fc88f1a18f9759b4a1f1b20e5cc75c487803d85
santiagolaiton <santiago.laitoncubides@hotmail.com>
2021-08-24 20:54:09 -0500

    Primer punto ok

---
+++ lab01/src/test/java/ieti/eci/lab01/lab01/Lab01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ieti.eci.lab01.lab01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
